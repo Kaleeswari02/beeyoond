780fb2c6790641c141d46233de4e9083e4ef1dfc
Kaleeswari02 <kaleeskaleeswari02@gmail.com>
2025-06-13 10:29:38 +0530

    changes

--- src/app/footer/page.js
+++ src/app/footer/page.js
@@ -2,20 +2,27 @@
 import React from "react";
 import styles from "./footer.module.css";
 import Link from "next/link";
+import { Icon } from '@iconify/react';
+
 
 const Footer = () => {
   return (
      <div className={`${styles.footerSection}`}>
-    <footer className={`${styles.footer} pt-5 pb-3`}>
-      <div className="container-fluid ">
-        <div  className={`${styles.imaSec} row align-items-start p-5 `}>
-          <div className="col-xl-1"></div>
-          <div className="col-md-4 col-xl-4 col-lg-4 mb-4 mb-md-0">
-            <img src="/assets/images/logo.png"  alt="Beeyoond Logo" className={`${styles.footerLogo} mb-3 img-fluid`}/>
+    <footer className={`${styles.footer}`}>
+      <div className={`container-fluid ${styles.customContainer}`}>
+        <div  className={`${styles.imaSec} row align-items-start mt-5`}>
+          <div className="col-md-4">
+            <a href="/">
+            <img src="/assets/images/logo.png" alt="Beeyoond Logo" className={`${styles.footerLogo} mb-3 img-fluid`}/></a>
             <div className="d-flex align-items-start mb-4">
-              <h1 className={styles.email}>info@beeyoondgaming.com</h1>
-              <img src="/assets/images/arrow-down.png" alt="email Logo" className={`${styles.downArrow}  img-fluid`}/>
+            <div
+              className={styles.emailWrapper}
+              onClick={() => (window.location.href = "mailto:info@beeyoondgaming.com")}
+            >
+              <span className={styles.emailText}>info@beeyoondgaming.com
+              <img src="/assets/images/arrow-down.png"  className={styles.emailArrow} /></span>
             </div>
+          </div>
             <div className="d-flex align-items-start mb-4">
               <img src="/assets/images/location.png" alt="email Logo" className={`${styles.icon} mb-3 img-fluid`}/>
               <span className={styles.text}>  Annai Therasa Street, Kalavasal, <br /> Madurai, Tamil Nadu 625016
@@ -27,7 +34,7 @@ const Footer = () => {
             </div>
           </div>
 
-          <div className="col-md-8 col-xl-7 col-lg-7">
+          <div className="col-md-8 mt-5">
             <div className={`d-flex flex-wrap ${styles.navLinks}`}>
               <Link href="/" className={styles.link}>Home</Link>
               <Link href="#about" className={styles.link}>About us</Link>
@@ -37,30 +44,31 @@ const Footer = () => {
               <Link href="#portfolio" className={styles.link}>Portfolio</Link>
               <div className="w-100 mt-2">
               <span className={styles.servicesText}>Services</span>
-
               </div>
-              <Link href="/services#2d3d" className={styles.link}>2D/3D Animation</Link>
-              <Link href="/services#video" className={styles.link}>Video Editing</Link>
-              <Link href="/services#ai" className={styles.link}>AI & Automation</Link>
-              <Link href="/services#game" className={styles.link}>Game Development</Link>
+              <p href="/services#2d3d" className={styles.link}>2D/3D Animation</p>
+              <p href="/services#video" className={styles.link}>Video Editing</p>
+              <p href="/services#ai" className={styles.link}>AI & Automation</p>
+              <p href="/services#game" className={styles.link}>Game Development</p>
               <Link href="/" className={styles.link}>Terms & Conditions</Link>
-              <Link href="/" className={styles.link}>Cookies</Link>
-              <div className="ms-auto d-flex gap-4 mt-2">
-                <Link href="https://in.linkedin.com/company/beeyoondgaming" className={styles.icon} target="_blank">
-                  <img src={`/assets/images/linkedin.png`} alt="linkedin" width={40} height={40} />
-                </Link>
-                <Link href="https://www.instagram.com/p/DJoxDdohR6f/" className={styles.icon} target="_blank">
-                  <img src={`/assets/images/instagram.png`} alt="linkedin" width={40} height={40} />
-                </Link>
-                <Link href="https://twitter.com/example" className={styles.icon} target="_blank">
-                 <img src={`/assets/images/twitter.png`} alt="linkedin" width={40} height={40} />
-                </Link>
+              <Link href="/" className={styles.link}>Privacy & Policy</Link>
+              <div className="ms-auto d-flex gap-5 mt-2">
+              <Link href="https://in.linkedin.com/company/beeyoondgaming" className={`${styles.icon} ${styles.iconHoverEffect}`} target="_blank">
+                <Icon icon="mingcute:linkedin-line" width="42" height="42" />
+              </Link>
+
+              <Link href="https://www.instagram.com/p/DJoxDdohR6f/" className={`${styles.icon} ${styles.iconHoverEffect}`} target="_blank">
+                <Icon icon="lets-icons:insta" width="40" height="40" />
+              </Link>
+
+              <Link href="https://twitter.com/example" className={`${styles.icon} ${styles.iconHoverEffect}`} target="_blank">
+                <Icon icon="prime:twitter" width="30" height="30" />
+              </Link>          
               </div>
             </div>
           </div>
         </div>
 
-        <div className="text-center mb-xl-5 mb-lg-5">
+        <div className="text-center mt-5">
           <p className={styles.copyright}>
             © 2025 <span>beeyoondgaming</span>. All
             rights reserved.
